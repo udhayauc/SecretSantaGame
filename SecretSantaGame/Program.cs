c428a7631b2686ed81aa2681685aff72f65a2a23
udhayauc <udhayachandran18062001@gmail.com>
2025-03-20 08:37:59 +0530

    Changes: File path and method calls

--- SecretSantaGame/Program.cs
+++ SecretSantaGame/Program.cs
@@ -1,2 +1,38 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.IO;
+using SecretSantaGame.Models;
+using SecretSantaGame.Services;
+using SecretSantaGame.Utils;
+
+class Program
+{
+    static void Main()
+    {
+        string employeesFile = "Employees.csv";
+        string lastYearFile = "last_year_assignments.csv";
+        string outputFile = "secret_santa_output.csv";
+
+        var csvService = new CsvParserService();
+        var secretSantaAssigner = new SecretSantaAssigner();
+
+        var employees = csvService.ReadEmployeesFromCsv(employeesFile);
+        var lastYearAssignments = FileHelper.ReadLastYearAssignments(lastYearFile);
+
+        if(employees.Count < 2)
+        {
+            Console.WriteLine("Not enough employees to conduct Secret Santa.");
+            return;
+        }
+        try
+        {
+            var assignments = secretSantaAssigner.AssignSecretSantas(employees, lastYearAssignments);
+            csvService.WriteAssignmentstoCsv(outputFile, assignments);
+
+            Console.WriteLine("Secret Santa assignments completed! Check output CSV.");
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Error: {ex.Message}");
+        }
+    }
+}
