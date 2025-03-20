731fae1eea5cbb4bf5c265bd585196e631aa7e38
udhayauc <udhayachandran18062001@gmail.com>
2025-03-20 08:36:19 +0530

    Services: CsvParser and Santa Assigner inclusion

---
+++ SecretSantaGame/Services/CsvParserService.cs
@@ -0,0 +1,52 @@
+﻿using CsvHelper;
+using CsvHelper.Configuration;
+using SecretSantaGame.Models;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SecretSantaGame.Services
+{
+    public class CsvParserService
+    {
+        public List<Employee> ReadEmployeesFromCsv(string filePath)
+        {
+            try
+            {
+                using var reader = new StreamReader(filePath);
+                using var csv = new CsvReader(reader, new CsvConfiguration(CultureInfo.InvariantCulture));
+
+                var employees = csv.GetRecords<Employee>().ToList();
+                return employees;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error reading CSV file: {ex.Message}");
+                return new List<Employee>();
+            }
+        }
+
+        public void WriteAssignmentstoCsv(string filePath, List<SecretSantaAssignment> assignments)
+        {
+            try
+            {
+                using var writer = new StreamWriter(filePath);
+                using var csv = new CsvWriter(writer, new CsvConfiguration(CultureInfo.InvariantCulture));
+
+                csv.WriteRecords(assignments.Select(a => new
+                {
+                    Santa_Name = a.Santa.Name,
+                    Santa_Email = a.Santa.Email,
+                    Secret_Child_Name = a.SecretChild.Name,
+                    Secret_Child_Email = a.SecretChild.Email,
+                }));
+            }
+            catch (Exception ex) {
+                Console.WriteLine($"Error writing CSV file: {ex.Message}");
+            }
+        }
+    }
+}
---
+++ SecretSantaGame/Services/SecretSantaAssigner.cs
@@ -0,0 +1,43 @@
+﻿using SecretSantaGame.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SecretSantaGame.Services
+{
+    public class SecretSantaAssigner
+    {
+        public List<SecretSantaAssignment> AssignSecretSantas(List<Employee> employees, Dictionary<string, string> lastYearAssignments)
+        {
+            try
+            {
+                var random = new Random();
+                var shuffledEmployees = employees.OrderBy(x => random.Next()).ToList();
+                var assignments = new List<SecretSantaAssignment>();
+
+                for(int i = 0; i < employees.Count; i++)
+                {
+                    var santa = employees[i];
+                    var possibleChildren = shuffledEmployees.Where(e => e.Email != santa.Email && (lastYearAssignments[santa.Email] != e.Email || 
+                    !lastYearAssignments.ContainsKey(santa.Email))).ToList();
+
+                    if(possibleChildren.Count == 0)
+                    {
+                        throw new InvalidOperationException("Valid Secret Santa assignment could not be made.");
+                    }
+
+                    var secretChild = possibleChildren[random.Next(possibleChildren.Count)];
+                    assignments.Add(new SecretSantaAssignment { Santa = santa, SecretChild = secretChild });
+                    
+                    shuffledEmployees.Remove(secretChild);
+                }
+                return assignments;
+            }
+            catch {
+                return new List<SecretSantaAssignment>();
+            }
+        }
+    }
+}
